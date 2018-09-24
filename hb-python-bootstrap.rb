cc94f15e295785e7988870e9613323497073dc28
patrick <patrick.fogarty@patanne.com>
2018-09-24 09:15:32 -0400

    revision

--- hb-python-bootstrap.rb
+++ hb-python-bootstrap.rb
@@ -4,6 +4,7 @@ class HbPythonBootstrap < Formula
 	url "http://san.customer.hb/repos.hb/macos/hb-python-bootstrap.tar.gz"
 	sha256 "dac1a6dd99f6e53585dcb646a6a704ce6949ef5eee6daf0a02f6671a06105beb"
 	version "0.0.1"
+	revision 1
   
 	depends_on "curl"
 
