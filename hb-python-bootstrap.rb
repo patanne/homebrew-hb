256518a94105de0d0f7d5094c1f54f2413d61e51
patrick <patrick.fogarty@patanne.com>
2018-09-24 10:15:50 -0400

    sitecustomization. take 1

--- hb-python-bootstrap.rb
+++ hb-python-bootstrap.rb
@@ -2,16 +2,21 @@ class HbPythonBootstrap < Formula
 	desc "hb-python-bootstrap"
 	homepage "https://github.com/"
 	url "http://san.customer.hb/repos.hb/macos/hb-python-bootstrap.tar.gz"
-	sha256 "45284cde76cdd865082fb60c2b140dfda0490d4f75b601de2554486cf8499516"
+	sha256 "dbb3dde0182335a087454ca8c6e355720d27a7144defa2f5bd5cae28256c9ce8"
 	version "0.0.1"
-	revision 3
+	revision 5
   
 	depends_on "curl"
 
 	bottle :unneeded
 
+	def sitepackages36
+		prefix/"lib/python3.6/site-packages"
+	end
+
 	def install
 		lib.install "hb-python-bootstrap"
+		sitepackages36.install "sitcustomization.txt"
 #		lib.install Dir["*"]
 	end
   end
