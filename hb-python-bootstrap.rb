81d77f321fdca7afeb888166bb963be610ae915e
patrick <patrick.fogarty@patanne.com>
2018-09-24 09:00:51 -0400

    initial commit

---
+++ hb-python-bootstrap.rb
@@ -0,0 +1,16 @@
+class hbootstrap < Formula
+	desc "hb-python-bootstrap"
+	homepage "https://github.com/"
+	url "http://san.customer.hb/repos.hb/macos/hb-python-bootstrap.tar.gz"
+	sha256 "dac1a6dd99f6e53585dcb646a6a704ce6949ef5eee6daf0a02f6671a06105beb"
+	version "0.0.1"
+  
+	depends_on "curl"
+
+	bottle :unneeded
+
+	def install
+		lib.install "hb-python-bootstrap"
+	end
+  end
+  
