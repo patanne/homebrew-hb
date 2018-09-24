0699f02bfe1c96b9158d81f5598157813c7035f8
patrick <patrick.fogarty@patanne.com>
2018-09-24 11:45:55 -0400

    multiple pythons

--- hb-python-bootstrap.rb
+++ hb-python-bootstrap.rb
@@ -4,7 +4,7 @@ class HbPythonBootstrap < Formula
 	url "http://san.customer.hb/repos.hb/macos/hb-python-bootstrap.tar.gz"
 	sha256 "dbb3dde0182335a087454ca8c6e355720d27a7144defa2f5bd5cae28256c9ce8"
 	version "0.0.1"
-	revision 19
+	revision 20
   
 	depends_on "curl"
 
@@ -16,6 +16,10 @@ class HbPythonBootstrap < Formula
 
 	def sitepackages36
 		prefix/"lib/python3.6/site-packages"
+	end
+
+	def sitepackages36live
+		HOMEBREW_PREFIX/"lib/python3.6/site-packages"
 	end
 
 	def sitepackages37
@@ -32,11 +36,12 @@ class HbPythonBootstrap < Formula
 			sitepackages34.install_symlink libexec/"sitecustomize.txt"
 		end
 
-		if Dir.exist?(HOMEBREW_PREFIX/"lib/python3.6/site-packages")
+		if sitepackages36live.exist?
 			sitepackages36.install_symlink libexec/"sitecustomize.txt" => "sitecustomize.py"
 		end
 
 		if Dir.exist?(HOMEBREW_PREFIX/"lib/python3.7/site-packages")
+#			if File.exist?()
 			sitepackages37.install_symlink libexec/"sitecustomize.txt"
 		end
 #		lib.install Dir["*"]
