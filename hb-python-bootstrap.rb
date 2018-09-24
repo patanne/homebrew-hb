988800d77821f094b2018756631aa3a167b62eb5
patrick <patrick.fogarty@patanne.com>
2018-09-24 10:52:18 -0400

    multiple pythons

--- hb-python-bootstrap.rb
+++ hb-python-bootstrap.rb
@@ -4,7 +4,7 @@ class HbPythonBootstrap < Formula
 	url "http://san.customer.hb/repos.hb/macos/hb-python-bootstrap.tar.gz"
 	sha256 "dbb3dde0182335a087454ca8c6e355720d27a7144defa2f5bd5cae28256c9ce8"
 	version "0.0.1"
-	revision 7
+	revision 8
   
 	depends_on "curl"
 
@@ -25,15 +25,15 @@ class HbPythonBootstrap < Formula
 	def install
 		lib.install "hb-python-bootstrap"
 
-		if sitepackages34.exists?
+		if sitepackages34.exist?
 			sitepackages34.install "sitecustomize.txt"
 		end
 
-		if sitepackages36.exists?
+		if sitepackages36.exist?
 			sitepackages36.install "sitecustomize.txt"
 		end
 
-		if sitepackages37.exists?
+		if sitepackages37.exist?
 			sitepackages37.install "sitecustomize.txt"
 		end
 #		lib.install Dir["*"]
