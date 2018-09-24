96d0d01c57f983007a7e15208296ed14f4b5d886
patrick <patrick.fogarty@patanne.com>
2018-09-24 09:28:36 -0400

    files in subfolder

--- hb-python-bootstrap.rb
+++ hb-python-bootstrap.rb
@@ -2,16 +2,17 @@ class HbPythonBootstrap < Formula
 	desc "hb-python-bootstrap"
 	homepage "https://github.com/"
 	url "http://san.customer.hb/repos.hb/macos/hb-python-bootstrap.tar.gz"
-	sha256 "dac1a6dd99f6e53585dcb646a6a704ce6949ef5eee6daf0a02f6671a06105beb"
+	sha256 "45284cde76cdd865082fb60c2b140dfda0490d4f75b601de2554486cf8499516"
 	version "0.0.1"
-	revision 2
+	revision 3
   
 	depends_on "curl"
 
 	bottle :unneeded
 
 	def install
-		lib.install Dir["*"]
+		lib.install "hb-python-bootstrap"
+#		lib.install Dir["*"]
 	end
   end
   
