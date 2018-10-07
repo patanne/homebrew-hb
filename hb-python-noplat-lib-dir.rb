c83fa3b86f1ad2ee9b948000646aec8b9aadedca
patrick <patrick.fogarty@patanne.com>
2018-10-07 16:46:54 -0400

    fix class name

--- hb-python-noplat-lib-dir.rb
+++ hb-python-noplat-lib-dir.rb
@@ -1,10 +1,10 @@
-class HbPythonBootstrapDir < Formula
-	desc "hb-python-bootstrap, the directory version"
+class HbPythonNoplatLibDir < Formula
+	desc "hb-python-noplat-lib, the directory version"
 	homepage "https://github.com/"
 	url "http://san.customer.hb/repos.hb/macos/homebrew/packages/hb-python-noplat-lib-0.0.0-dir.tar.gz"
 	sha256 "66416e8d75d291c4d798f2ef93647470c4ec1ad235321a698ed3bd99f0db32ed"
 	version "0.0.0"
-	revision 0
+	revision 1
   
 	depends_on "curl"
 	depends_on "python37"
