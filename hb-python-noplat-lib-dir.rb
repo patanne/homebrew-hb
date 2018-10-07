03c31df76c3ba62e5299d8774493d648d2b9ce9d
patrick <patrick.fogarty@patanne.com>
2018-10-07 16:44:20 -0400

    first commit

---
+++ hb-python-noplat-lib-dir.rb
@@ -0,0 +1,18 @@
+class HbPythonBootstrapDir < Formula
+	desc "hb-python-bootstrap, the directory version"
+	homepage "https://github.com/"
+	url "http://san.customer.hb/repos.hb/macos/homebrew/packages/hb-python-noplat-lib-0.0.0-dir.tar.gz"
+	sha256 "66416e8d75d291c4d798f2ef93647470c4ec1ad235321a698ed3bd99f0db32ed"
+	version "0.0.0"
+	revision 0
+  
+	depends_on "curl"
+	depends_on "python37"
+
+	bottle :unneeded
+
+	def install
+		lib.install "hb-python-noplat-lib-0.0.0"
+	end
+  end
+  
